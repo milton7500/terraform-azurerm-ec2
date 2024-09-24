3bd16234a66ec4fb73af3189434f4a4ffd751c21
Rajput7500 <132784995+milton7500@users.noreply.github.com>
2024-09-24 21:21:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0ec0e125bb6c6e8ec"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name = "HelloWorld"
   }
